fcaa2b22d6d30ca8a3fe144ab957f25e8d66d7a4
serii burduja <seriiburduja@gmail.com>
2024-10-15 13:19:45 +0300

    feat: finished nato project

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
-import random
-# names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
-#
-# students_score = {item:random.randint(1, 100) for item in names}
-# print(f"students_score: {students_score}")
-# students_score = {key:value for key, value in students_score.items() if value > 60}
-# print(f"students_score: {students_score}")
+import pandas
 
+data = pandas.read_csv("nato.csv")
+nato_dict = {row.letter:row.code for (_, row) in data.iterrows()}
 
-# sentence = 'What is the Airspeed Velocity of an Unladen Swallow'
-# print(f"sentence: {sentence}")
-#
-# words = sentence.split(' ')
-# print(f"words: {words}")
-#
-# result = {item:len(item) for item in words}
-# print(f"result: {result}")
-
-def celsToFar(t):
-    return (t * 9/5) + 32
-
-
-weather_c = {
-        "Wednesday": 15,
-        "Thursday": 14,
-        "Friday": 21,
-        "Saturday": 22,
-        "Sunday": 24,
-        }
-
-weather_f = {key:celsToFar(value) for key,value in weather_c.items()}
-print(f"weather_f: {weather_f}")
+user_input = input("Enter a word: ").upper()
+if user_input != '':
+    words = [nato_dict[item] for item in user_input]
+    print(f"words: {words}")
---
+++ test.py
@@ -0,0 +1,45 @@
+import random
+import pandas
+# names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+#
+# students_score = {item:random.randint(1, 100) for item in names}
+# print(f"students_score: {students_score}")
+# students_score = {key:value for key, value in students_score.items() if value > 60}
+# print(f"students_score: {students_score}")
+
+
+# sentence = 'What is the Airspeed Velocity of an Unladen Swallow'
+# print(f"sentence: {sentence}")
+#
+# words = sentence.split(' ')
+# print(f"words: {words}")
+#
+# result = {item:len(item) for item in words}
+# print(f"result: {result}")
+
+# def celsToFar(t):
+#     return (t * 9/5) + 32
+#
+#
+# weather_c = {
+#         "Wednesday": 15,
+#         "Thursday": 14,
+#         "Friday": 21,
+#         "Saturday": 22,
+#         "Sunday": 24,
+#         }
+#
+# weather_f = {key:celsToFar(value) for key,value in weather_c.items()}
+# print(f"weather_f: {weather_f}")
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(f"student_data_frame: {student_data_frame}")
+
+for (index, row) in student_data_frame.iterrows():
+    print(f"row: {row.student}")
+
