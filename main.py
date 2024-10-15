33f7aed9b263d32662217e4526e4804aea3cf982
serii burduja <seriiburduja@gmail.com>
2024-10-15 12:42:08 +0300

    feat: temp c to f

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-numbers = [3, 2,3, 5, 8,13, 20, 33, 55]
+import random
+# names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+#
+# students_score = {item:random.randint(1, 100) for item in names}
+# print(f"students_score: {students_score}")
+# students_score = {key:value for key, value in students_score.items() if value > 60}
+# print(f"students_score: {students_score}")
 
-even_numbers = [n for n in numbers if n % 2 == 0]
-print(f"even_numbers: {even_numbers}")
+
+# sentence = 'What is the Airspeed Velocity of an Unladen Swallow'
+# print(f"sentence: {sentence}")
+#
+# words = sentence.split(' ')
+# print(f"words: {words}")
+#
+# result = {item:len(item) for item in words}
+# print(f"result: {result}")
+
+def celsToFar(t):
+    return (t * 9/5) + 32
+
+
+weather_c = {
+        "Wednesday": 15,
+        "Thursday": 14,
+        "Friday": 21,
+        "Saturday": 22,
+        "Sunday": 24,
+        }
+
+weather_f = {key:celsToFar(value) for key,value in weather_c.items()}
+print(f"weather_f: {weather_f}")
