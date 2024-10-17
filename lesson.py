c3e5025269c30cdb0ccfca91205c120ac505a5cd
serii burduja <seriiburduja@gmail.com>
2024-10-17 08:31:47 +0300

    feat: error check

--- lesson.py
+++ lesson.py
@@ -1,13 +1,18 @@
 def lesson():
-    fruits = ['apple', 'banana', 'cherry']
-
-    def make_pie(index):
+    facebook_posts = [
+            {"Likes": 21, "Comments": 2},
+            {"Likes": 13, "Comments": 2},
+            {"Likes": 33, "Comments": 8},
+            { "Comments": 1},
+            { "Comments": 1},
+            {"Likes": 3, "Comments": 1},
+            ]
+    total_likes = 0
+    for post in facebook_posts:
         try:
-            fruit = fruits[index]
-        except IndexError:
-            print("Fruit pie")
+            total_likes += post["Likes"]
+        except KeyError as error_message:
+            print(f"Key doesn't exists: {error_message}")
         else:
-            print(fruit + " pie")
-
-    make_pie(2)
-    # make_pie(4)
+            print(f"Post processed successfully")
+    print(f"Total likes: {total_likes}")
--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pandas
 
 data = pandas.read_csv("nato.csv")
 nato_dict = {row.letter:row.code for (_, row) in data.iterrows()}
+print(f"nato_dict: {nato_dict}")
 
-user_input = input("Enter a word: ").upper()
-if user_input != '':
-    words = [nato_dict[item] for item in user_input]
-    print(f"words: {words}")
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    if user_input != '':
+        try:
+            words = [nato_dict[item] for item in user_input]
+        except KeyError as error_message:
+            print("Please enter only letters")
+            generate_phonetic()
+        else:
+            print(f"words: {words}")
+
+generate_phonetic()
 
 # from lesson import lesson
 # lesson()
